2da3e85fe0259dc8f614ee740cea87e7abac64ca
ville <ville.prami@aalto.fi>
2023-06-30 23:30:25 +0300

    Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import "./index.css"
 import { Provider } from "react-redux"
 import store from "./store"
 
+import { inject } from '@vercel/analytics';
+ 
+inject();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <App />
